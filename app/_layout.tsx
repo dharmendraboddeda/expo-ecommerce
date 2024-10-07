58575738509e2d76bf39fb98bb87c1924e656916
Dharmendra Boddeda <dharmendra.developer6@gmail.com>
2024-10-07 12:39:11 +0530

    themeing completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,42 @@
 import { Stack } from "expo-router";
+import {
+  PaperProvider,
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from "react-native-paper";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
+import { useColorScheme } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+
+  const navigationTheme = adaptNavigationTheme({
+    reactNavigationDark: DefaultTheme,
+    reactNavigationLight: DefaultTheme,
+  });
+
+  const paperTheme =
+    colorScheme === "dark"
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <PaperProvider theme={paperTheme}>
+      <NavigationContainer
+        theme={
+          colorScheme === "dark"
+            ? navigationTheme.DarkTheme
+            : navigationTheme.LightTheme
+        }
+        independent={true}
+      >
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+        </Stack>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
 import { Text, View } from "react-native";
+import { useTheme } from "react-native-paper";
 
 export default function Index() {
+  const theme = useTheme();
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: theme.colors.background,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ color: theme.colors.onBackground }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
